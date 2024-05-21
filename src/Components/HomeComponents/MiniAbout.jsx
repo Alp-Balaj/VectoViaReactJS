62e8fba5b8f02b50f081980d0cfedef5cfba6064
Alp-Balaj <alpbalaj1203@gmail.com>
2024-05-21 18:20:46 +0200

    EhUreHjeka

--- src/Components/HomeComponents/MiniAbout.jsx
+++ src/Components/HomeComponents/MiniAbout.jsx
@@ -8,17 +8,34 @@ import easy from '../../Assets/logos/easy-to-use.png';
 import free from '../../Assets/logos/free.png';
 import shield from '../../Assets/logos/shield.png';
 
+const Card = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 const Item = styled.div`
-    height:100px;
-    border: 3px solid #000;
+    height: 450px;
+    width: 80%;
+    padding: 25px 10px;
+    border: 1px solid #000;
     border-radius: 15px;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    box-shadow: 0px 5px 8px rgba(0, 0, 0, 0.2);
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+    &:hover {
+        transform: scale(1.05);
+        box-shadow: 0px 10px 16px rgba(0, 0, 0, 0.3);
+    }
+    min-height: 300px;
+    max-height: 600px;
+    justify-content: space-around;
     img{
+        margin-top: 20px;
         width: 25%;
-        height: 80%;
-        border-left: 2px solid #000;
+        height: 30%;
     }
 `;
 
@@ -27,11 +44,12 @@ const TextPart = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: start;
-    width: 75%;
+    width: 100%;
     padding: 10px 15px;
-    text-align: start;
+    text-align: center;
+
     h6{
-        padding-top: 10px;
+        font-size: 30px;
         color: #FFC800;
         text-transform: uppercase;
     }
@@ -46,59 +64,75 @@ const MiniAbout = () => {
                     <h1 className="section-heading text-uppercase">Why choose us</h1>
                     <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                     <Grid container spacing={2}>
+                      {/* <Grid item xs={12} md={1}></Grid> */}
+
                       <Grid item xs={12} md={4}>
-                        <Item>
-                            <TextPart>
-                                <h6>24/7 service</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur</p>
-                            </TextPart>
-                            <img src={headphones} alt="fuck" />
-                        </Item>
+                        <Card>
+                            <Item>
+                                <img src={headphones} alt="fuck" />
+                                <TextPart>
+                                    <h6>24/7 service</h6>
+                                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                                </TextPart>
+                            </Item>
+                        </Card>
                       </Grid>
                       <Grid item xs={12} md={4}>
-                      <Item>
-                            <TextPart>
-                                <h6>Easy to use</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur</p>
-                            </TextPart>
-                            <img src={easy} alt="fuck" />
-                        </Item>
+                        <Card>
+                            <Item>
+                                <img src={easy} alt="Easy to use" />
+                                <TextPart>
+                                    <h6>Easy to use</h6>
+                                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                                </TextPart>
+                            </Item>
+                        </Card>
                       </Grid>
                       <Grid item xs={12} md={4}>
-                      <Item>
-                            <TextPart>
-                                <h6>Online access</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur</p>
-                            </TextPart>
-                            <img src={laptop} alt="fuck" />
-                        </Item>
+                        <Card>
+                            <Item>
+                                <img src={laptop} alt="fuck" />
+                                <TextPart>
+                                    <h6>Online access</h6>
+                                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                                </TextPart>
+                            </Item>
+                        </Card>
+                      </Grid>
+                      {/* <Grid item xs={12} md={1}></Grid> */}
+
+                      <Grid item xs={12} md={4}>
+                        <Card>
+                            <Item>
+                                <img src={leaves} alt="fuck" />
+                                <TextPart>
+                                    <h6>Eco Friendly</h6>
+                                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                                </TextPart>
+                            </Item>
+                        </Card>
                       </Grid>
                       <Grid item xs={12} md={4}>
-                      <Item>
-                            <TextPart>
-                                <h6>Eco Friendly</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur</p>
-                            </TextPart>
-                            <img src={leaves} alt="fuck" />
-                        </Item>
+                        <Card>
+                            <Item>
+                                <img src={free} alt="fuck" />
+                                <TextPart>
+                                    <h6>It's Free</h6>
+                                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                                </TextPart>
+                            </Item>
+                        </Card>
                       </Grid>
                       <Grid item xs={12} md={4}>
-                      <Item>
-                            <TextPart>
-                                <h6>It's Free</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur</p>
-                            </TextPart>
-                            <img src={free} alt="fuck" />
-                        </Item>
-                      </Grid>
-                      <Grid item xs={12} md={4}>
-                      <Item>
-                            <TextPart>
-                                <h6>We value safety</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur</p>
-                            </TextPart>
-                            <img src={shield} alt="fuck" />
-                        </Item>
+                        <Card>
+                            <Item>
+                                <img src={shield} alt="fuck" />
+                                <TextPart>
+                                    <h6>We value safety</h6>
+                                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                                </TextPart>
+                            </Item>
+                        </Card>
                       </Grid>
                     </Grid>
                 </div>
