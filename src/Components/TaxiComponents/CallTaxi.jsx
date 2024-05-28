8c82b7ab4905652d77dcf2ab74aad262ea3ed911
Alp-Balaj <alpbalaj1203@gmail.com>
2024-05-28 15:43:22 +0200

    Taxi starting

--- src/Components/TaxiComponents/CallTaxi.jsx
+++ src/Components/TaxiComponents/CallTaxi.jsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import {Container, Grid} from '@mui/material';
-import styled from 'styled-components';
+import {React, useEffect} from "react";
+import { Container, Grid } from "@mui/material";
+import styled from "styled-components";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const Calls = styled.div`
+  background-color: #fff;
+  padding-top: 150px;
+  padding-bottom: 150px;
+`;
 
 const Card = styled.div`
-    
+  background-color: #ebe8e8;
+  width: 100%;
+  height: 450px;
+  border-radius: 10px;
+  .Image{
+    width: 50%;
+    height:100%;
+  }
+  .Text{
+    width: 50%;
+    height:100%;
+  }
+`;
+
+const H3 = styled.div`
+  font-size: 1rem;
+  font-weight: 400;
+  font-style: italic;
+  font-family: "Roboto Slab", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  margin-bottom: 4rem;
+  --bs-text-opacity: 1;
+  color: #6c757d !important;
 `
 
-const CallTaxi = () => {
-  return (
-    <div>
-        <Container maxWidth="xl">
-            <Grid container spacing={3}>
-                <Grid xs={12} md={12}>
-                    <Card>
 
-                    </Card>
-                </Grid>
-            </Grid>
-        </Container>
-    </div>
-  )
-}
+const CallTaxi = () => {
+    useEffect(() => {
+        AOS.init();
+      }, [])
+  return (
+    <Calls>
+      <div className="container">
+        <div className="text-center">
+          <h2 data-aos="zoom-out" className="section-heading text-uppercase">
+            Call a Taxi!
+          </h2>
+          <H3 data-aos="zoom-out">
+            You wont be dissapointed.
+          </H3>
+        </div>
+      </div>
+      <Container maxWidth="xl">
+        <Grid container spacing={3}>
+          <Grid xs={12} md={12}>
+            <Card>
+                <div className="Image">
+                    <img src="" alt="" />
+                </div>
+                <div className="Text">
+
+                </div>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </Calls>
+  );
+};
 
 export default CallTaxi;
